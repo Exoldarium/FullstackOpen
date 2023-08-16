f48acd2b46fab02b0cd2e2ccdf5ea87e0d59df29
Dusan <shandoo91@gmail.com>
2023-08-16 17:00:17 +0700

    part9 bmi calculator

---
+++ part9/typescript/bmiCalculator.ts
@@ -0,0 +1,47 @@
+// BMI = mass/height(squared)
+// A BMI of 25.0 or more is overweight, while the healthy range is 18.5 to 24.9
+interface DivideValues {
+  value1: number;
+  value2: number;
+}
+
+function parseArguments(args: string[]): DivideValues {
+  if (args.length < 4) throw new Error('Too few arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      value1: Number(args[3]),
+      value2: Number(args[2])
+    }
+  } else {
+    throw new Error('Provided values must be numbers');
+  }
+}
+
+function calculateBmi(weight: number, height: number): string {
+  const total = weight / Math.pow(height / 100, 2);
+
+  if (total < 18.5) {
+    return 'Underweight';
+  } else if (total > 24.9) {
+    return 'Overweight';
+  } else {
+    return 'Normal (healthy weight)';
+  }
+}
+
+try {
+  const { value1, value2 } = parseArguments(process.argv);
+  calculateBmi(value1, value2);
+} catch (error) {
+  let errorMessage = 'Something bad happened';
+  if (error instanceof Error) {
+    errorMessage += 'Error' + error.message;
+  }
+  console.log(error.message);
+}
+
+const weight: number = Number(process.argv[3]);
+const height: number = Number(process.argv[2]);
+console.log(calculateBmi(weight, height));
